a97c72549774c3a6dfc00be2c2cf6f7c82616ad4
Andi Fahmi <andifahmi266@gmail.com>
2023-02-01 15:03:48 +0800

    Modified Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 const Home = () => {
     return (
         <div
-            name='home'
+            name='home' 
             className='h-screen w-full bg-gradient-to-b from-black via-black  to-gray-800 '>
             <div
                 className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full md:flex-row'>
