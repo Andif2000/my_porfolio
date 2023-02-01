bae8786011338a410a4179a5269da65de09db491
Andi Fahmi <andifahmi266@gmail.com>
2023-02-01 15:04:06 +0800

    Modified App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import SocialLink from "./components/SocialLink";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <SocialLink/>
     </div>
   );
 }
