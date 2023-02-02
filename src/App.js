47630a9114f8aef477326ddf8c62e876a8569508
Andi Fahmi <andifahmi266@gmail.com>
2023-02-02 13:04:06 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import SocialLink from "./components/SocialLink";
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Home/>
+      <About/>
       <SocialLink/>
     </div>
   );
