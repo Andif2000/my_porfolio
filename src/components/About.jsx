5ba09e06f468708201c4db51e0bb2849792c8232
Andi Fahmi <andifahmi266@gmail.com>
2023-02-02 13:03:05 +0800

    Crated About Component

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div
+            name='about'
+            className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+                <div>
+                    <p>About</p>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore amet quod tenetur ullam perferendis pariatur eaque omnis rerum. Ratione, vel ullam, ipsa nobis ad quos non molestias officiis illum delectus magni, ea velit odit quia aliquam id. Facere reiciendis dicta soluta voluptatibus omnis, ad perferendis? Quos aliquam recusandae ratione assumenda!
+                </p>
+                <br />
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae ipsum officia, eos iure, perspiciatis quis cumque libero sapiente ad est eum modi, dicta saepe! Omnis dignissimos fugit numquam maiores laboriosam commodi ratione nihil ullam voluptatem suscipit, quibusdam repellendus odit sint temporibus voluptatum reprehenderit nostrum vel porro, veniam facilis similique. Non!</p>
+            </div>
+        </div>
+    )
+}
+
+export default About
