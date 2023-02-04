7aa817c90e8b84e03ae2cb0beb01a3bab90a2c87
Andi Fahmi <andifahmi266@gmail.com>
2023-02-04 11:43:18 +0800

    Modified Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -59,7 +59,7 @@ const Experience = () => {
                         techs.map(({ id, title, src, style }) => (
                             <div key={id} className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg '+style}>
                                 <img src={src} alt="" className='w-20 mx-auto' />
-                                <p>{title}</p>
+                                <p className='mt-2'>{title}</p>
                             </div>
                         ))
                     }
