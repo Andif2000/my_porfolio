1386a9721f5420c1ce916eb428709eb472a364d1
Andi Fahmi <andifahmi266@gmail.com>
2023-03-07 11:42:05 +0800

    Tidy Up

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -50,7 +50,7 @@ const Experience = () => {
             className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
             <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center h-full w-full text-white'>
                 <div>
-                    <p className='p-2 text-4xl font-bold border-b-4 bg-gray-500 inline'>Experience</p>
+                    <p className='p-2 text-4xl font-bold border-b-4 bg-gray-500 inline'>Experiences</p>
                     <p className='py-6'>These are the technologies I've worked with.</p>
                 </div>
 
