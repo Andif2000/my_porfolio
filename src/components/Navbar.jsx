4e6ba13e36d541028d41c8b0ea061221694a4a3e
Andi Fahmi <andifahmi266@gmail.com>
2023-03-25 22:23:44 +0800

    Tidy Up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar() {
             <ul className="hidden md:flex">
                 {links.map(({ id, link }) => (
                     <li key={id}
-                        className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
+                        className='px-4 capitalize cursor-pointer font-medium text-gray-500 hover:scale-105 duration-200'>
                         <Link to={link} smooth duration={500}>
                             {link}
                         </Link>
