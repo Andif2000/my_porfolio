2cc18e7d12a392a9774dd2dd096bddaa545f7625
Andi Fahmi <andifahmi266@gmail.com>
2023-03-29 13:45:11 +0800

    Tidy Up

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
-                    <p className='text-4xl inline border-b-4 font-bold  border-gray-500'>About</p>
+                    <p className='text-4xl border-b-4 inline font-bold  border-gray-500'>About</p>
                 </div>
                 <p className='text-xl mt-20'>
                     Andi Zulfahmi, Born in Maradda June 20 2000. Is an Alumni
